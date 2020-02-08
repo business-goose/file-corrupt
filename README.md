39b2cbdd155f06637d1e78314809f25e8bf3f97e
arclicious <44676012+arclicious@users.noreply.github.com>
2020-02-08 12:07:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# file-corrupt
+ A python script which corrupts files in a directory, or just a single file.
---
+++ file_corrupt.py
@@ -0,0 +1,44 @@
+import io
+import argparse
+import asyncio
+from secrets import randbelow
+from os import remove
+from os import path
+from os import listdir
+from shutil import rmtree
+
+async def delete(file,dir):
+    if str(dir) == "None" and str(file) != "None":
+        print("Opening file...")
+        f = open(file,"w")
+        print("Opened file, corrupting...")
+        for i in range(2500000):
+            f.write(chr(randbelow(127)))
+        f.close()
+        print("Overwritten, deleting...")
+    elif str(file) == "None":
+        index = 0
+        for filename in listdir(dir):
+            index += 1
+            print("Corrupting {} ({})...".format(filename,index))
+            filename = path.join(dir,filename)
+            try:
+                f = open(filename,"w")
+                for i in range(2500000):
+                    f.write(chr(randbelow(127)))
+                f.close()
+            except:
+                print("Running coroutine to corrupt the files in {}...".format(filename))
+                await delete(None,filename)
+
+                
+        print("Deleting directory...")
+        rmtree(dir)
+        print("Completed!")
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-f","--file",required=False, help="file to securely delete")
+ap.add_argument("-d","--directory",required=False,help="directory to securely delete")
+args = vars(ap.parse_args())
+asyncio.run(delete(args["file"],args["directory"]))
